6f4745c453a8a418d7b016bcd2fa20759ab19be0
todesking <discommunicative@gmail.com>
2014-04-23 00:40:13 +0900

    more test

--- spec/patm_spec.rb
+++ spec/patm_spec.rb
@@ -1,6 +1,22 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'patm.rb')
 
+module PatmHelper
+  extend RSpec::Matchers::DSL
+
+  matcher :match_to do|expected|
+    match do|actual|
+      actual.execute(Patm::Match.new, expected)
+    end
+  end
+
+  def pat(p)
+    Patm::Pattern.build_from(p)
+  end
+end
+
 describe Patm do
+  include PatmHelper
+
   it 'should do pattern matching with case-when expression' do
     p = Patm
     case [1, 2, 3]
@@ -10,5 +26,11 @@ describe Patm do
       []
     end
       .should == [2, 3]
+  end
+
+  describe '::Pattern' do
+    subject { pat(1) }
+    it { should match_to(1) }
+    it { should_not match_to(2) }
   end
 end
